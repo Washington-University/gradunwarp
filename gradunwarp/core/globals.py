6603919642fd671df31251451e2164d6d287e945
Tim Coalson <coalsont@wustl.edu>
2024-12-23 17:51:06 -0600

    set version 1.2.3

--- gradunwarp/core/globals.py
+++ gradunwarp/core/globals.py
@@ -6,7 +6,7 @@
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 import logging
 
-VERSION = '1.2.2'
+VERSION = '1.2.3'
 
 usage = '''
 gradient_unwarp infile outfile manufacturer -g <coefficient file> [optional arguments]
